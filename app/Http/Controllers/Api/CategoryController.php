95522dce799025174af8bb7b044b5c79095d17e2
PovilasKorop <povilask007@gmail.com>
2021-07-26 07:12:01 +0300

    First Eloquent API Resource

--- app/Http/Controllers/Api/CategoryController.php
+++ app/Http/Controllers/Api/CategoryController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers\Api;
 
 use App\Http\Controllers\Controller;
+use App\Http\Resources\CategoryResource;
 use App\Models\Category;
 use Illuminate\Http\Request;
 
@@ -15,7 +16,7 @@ class CategoryController extends Controller
      */
     public function index()
     {
-        return Category::all();
+        return CategoryResource::collection(Category::select('id', 'name')->get());
     }
 
     /**
