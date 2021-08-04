46447c1f42fd84b2cabec5482f64c85ba3b305bb
PovilasKorop <povilask007@gmail.com>
2021-08-04 10:35:35 +0300

    First Sanctum Token

--- routes/api.php
+++ routes/api.php
@@ -18,8 +18,29 @@ Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
 
-Route::apiResource('categories',
-    \App\Http\Controllers\Api\CategoryController::class);
+Route::group(['middleware' => 'auth:sanctum'], function() {
+    Route::apiResource('categories',
+        \App\Http\Controllers\Api\CategoryController::class);
 
-Route::apiResource('transactions',
-    \App\Http\Controllers\Api\TransactionController::class);
+    Route::apiResource('transactions',
+        \App\Http\Controllers\Api\TransactionController::class);
+});
+
+Route::post('/sanctum/token', function (Request $request) {
+    $request->validate([
+        'email' => 'required|email',
+        'password' => 'required',
+        'device_name' => 'required',
+    ]);
+
+    $user = \App\Models\User::where('email', $request->email)->first();
+
+    if (! $user || ! \Illuminate\Support\Facades\Hash::check($request->password, $user->password)) {
+        throw \Illuminate\Validation\ValidationException::withMessages([
+            'email' => ['The provided credentials are incorrect.'],
+        ]);
+    }
+
+    return $user->createToken($request->device_name)->plainTextToken;
+});
+
